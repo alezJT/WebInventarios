d12241d8176ede99b4b7387c985eb3e8e0d69538
MDSOFT <MDSOFT@DESKTOP-N76UMB6>
2023-02-07 08:19:53 -0400

    Stars Almacenes

---
+++ Models/Almacenes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebInventarios.Models
+{
+    public class Almacenes
+    {
+        [Display (Name ="Id del Almacen")]
+        public int IDAlmacen { get; set; }
+
+        [Display(Name = "Descripcion del Almacen")]
+        public string? DescripcionAlmacen { get; set; }
+
+        [Display(Name = "Referencia del Almacen")]
+        public string? ReferenciaAmacen { get; set; }
+    }
+}
