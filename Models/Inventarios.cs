a36bd3952a6fbbeec9e790ce65f3bf6ad83464f5
MDSOFT <MDSOFT@DESKTOP-N76UMB6>
2023-03-08 19:58:27 -0400

    vamos a poner el home de la tabla inventarios

--- Models/Inventarios.cs
+++ Models/Inventarios.cs
@@ -3,6 +3,9 @@
     public class Inventarios
     {
         public int ProductoId { get; set; }
+
+        public int IDAlmacen { get; set; }
         public int CantidadInv { get; set; }
+
     }
 }
