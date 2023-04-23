6ffde24411fa2ff12cdd3cf81cd593df251e08c1
Alezj <thebrother2011@hotmail.com>
2023-04-23 19:52:38 -0400

    usuario login

---
+++ Models/Usuarios.cs
@@ -0,0 +1,11 @@
+﻿namespace WebInventarios.Models
+{
+    public class Usuarios
+    {
+        public int IdUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string UsuarioNombre { get; set; }
+        public string Clave { get; set; }
+
+    }
+}
