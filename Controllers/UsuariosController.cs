8fed5231c849df2a1548fff7db3c211654254276
Alezj <thebrother2011@hotmail.com>
2023-04-24 13:58:39 -0400

    Avences en la pantalla de Usuarios

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebInventarios.Models;
+
+namespace WebInventarios.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private readonly ConexionContext _context;
+
+        public UsuariosController(ConexionContext context)
+        {
+            _context = context;
+        }
+      
+        public async Task <IActionResult> Index()
+        {
+            return View( await _context.Usuarios.ToListAsync());
+        }
+
+        // GET: UsuariosController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: UsuariosController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UsuariosController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UsuariosController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
