39fa927544c88418670ad70b79190e83e7f70cd8
Alezj <thebrother2011@hotmail.com>
2024-02-18 21:09:06 -0400

    Vamo a meter login

---
+++ Controllers/CuentasControllercs.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebInventarios.Controllers
+{
+    public class CuentasControllercs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+
+}
