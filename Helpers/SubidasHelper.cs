e83630535b4e3b66d787c5b5d3ccb38d3dc725c9
MDSOFT <MDSOFT@DESKTOP-N76UMB6>
2024-03-05 08:13:18 -0400

    Guardando archivo en disco

--- Helpers/SubidasHelper.cs
+++ Helpers/SubidasHelper.cs
@@ -1,10 +1,34 @@
-﻿namespace WebInventarios.Helpers
+﻿
+namespace WebInventarios.Helpers
 {
     public class SubidasHelper : ISubidasHelper
     {
+        private readonly string _Carpeta;
 
+        public SubidasHelper(String Carpeta)
+        {
+            _Carpeta = Carpeta;
+        }
 
+        public async Task<string>GuardarArchivoAsync(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                throw new ArgumentException("El archivo no es valido");
 
+            // gerenar Nonbre del Archivo
+            string nombreArchivo = Guid.NewGuid().ToString()+ Path.GetExtension(file.FileName);
+
+            // Ruta completa del archivo
+            string rutaCompleta = Path.Combine(_Carpeta, nombreArchivo);
+
+            // Guardar el archivo en disco
+            using (var stream = new FileStream(rutaCompleta, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return nombreArchivo; // Retorna el nombre del archivo Guardado
+        }
 
     }
 }
